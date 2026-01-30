6835cd27a50b33b942ca54252aaa071d8937cfad
Snarky <sparkykueken@gmail.com>
2026-01-30 09:22:08 +0100

    fix item table

--- worlds/sc2/item/item_tables.py
+++ worlds/sc2/item/item_tables.py
@@ -1506,7 +1506,7 @@ item_table = {
         ItemData(390 + SC2HOTS_ITEM_ID_OFFSET, ZergItemType.Mutation_5, 17, SC2Race.ZERG, parent=item_names.BULLFROG),
     item_names.SPORE_CRAWLER_BIO_BONUS: 
         ItemData(391 + SC2HOTS_ITEM_ID_OFFSET, ZergItemType.Mutation_5, 18, SC2Race.ZERG, parent=item_names.SPORE_CRAWLER),
-    item_names.SPORE_CRAWLER_BIO_BONUS: 
+    item_names.CORRUPTOR_ACID_SPLASH: 
         ItemData(392 + SC2HOTS_ITEM_ID_OFFSET, ZergItemType.Mutation_5, 19, SC2Race.ZERG, parent=item_names.CORRUPTOR),
 
     item_names.KERRIGAN_KINETIC_BLAST: ItemData(400 + SC2HOTS_ITEM_ID_OFFSET, ZergItemType.Ability, 0, SC2Race.ZERG, classification=ItemClassification.progression),
