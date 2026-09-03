47f3a1c31c3166130fcbd88ec10ddcaab1ce66ae
Snarky <sparkykueken@gmail.com>
2026-09-03 12:45:02 +0200

    add mutator options

--- worlds/sc2/item/item_tables.py
+++ worlds/sc2/item/item_tables.py
@@ -1077,11 +1077,15 @@ item_table = {
         ItemData(850 + SC2WOL_ITEM_ID_OFFSET, FactionlessItemType.MaxSupplyTrap, -1, SC2Race.ANY, quantity=0,
                  classification=ItemClassification.trap),
 
-    item_names.TRAP_GHOST_SPAWN:
-        ItemData(851 + SC2WOL_ITEM_ID_OFFSET, FactionlessItemType.GhostSpawnTrap, 0, SC2Race.ANY, quantity=5,
+    # Mutators
+    item_names.MUTATOR_GHOST_SPAWN:
+        ItemData(851 + SC2WOL_ITEM_ID_OFFSET, FactionlessItemType.MutatorGhostSpawn, 0, SC2Race.ANY, quantity=5,
                  classification=ItemClassification.trap),
-    item_names.TRAP_VOID_DUPLICATE:
-        ItemData(852 + SC2WOL_ITEM_ID_OFFSET, FactionlessItemType.VoidDuplicateTrap, 0, SC2Race.ANY, quantity=5,
+    item_names.MUTATOR_VOID_DUPLICATE:
+        ItemData(852 + SC2WOL_ITEM_ID_OFFSET, FactionlessItemType.MutatorVoidDuplicate, 0, SC2Race.ANY, quantity=5,
+                 classification=ItemClassification.trap),
+    item_names.MUTATOR_ENABLE_CLOAK:
+        ItemData(853 + SC2WOL_ITEM_ID_OFFSET, FactionlessItemType.MutatorEnableCloak, 0, SC2Race.ANY,
                  classification=ItemClassification.trap),
 
     # Nova gear
