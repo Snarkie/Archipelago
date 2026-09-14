6d9a9a2101565df7db828bd01bc60c42a97169eb
Snarky <sparkykueken@gmail.com>
2026-09-14 22:50:59 +0200

    [SC2] Add mutator options

--- worlds/sc2/item/item_tables.py
+++ worlds/sc2/item/item_tables.py
@@ -1077,11 +1077,15 @@ item_table = {
         ItemData(850 + SC2WOL_ITEM_ID_OFFSET, FactionlessItemType.MaxSupplyTrap, -1, SC2Race.ANY, quantity=0,
                  classification=ItemClassification.trap),
 
-    item_names.TRAP_GHOST_SPAWN:
-        ItemData(851 + SC2WOL_ITEM_ID_OFFSET, FactionlessItemType.GhostSpawnTrap, 0, SC2Race.ANY, quantity=5,
+    # Mutators
+    item_names.MUTATOR_GHOST_SPAWN:
+        ItemData(851 + SC2WOL_ITEM_ID_OFFSET, FactionlessItemType.MutatorGhostSpawn, 0, SC2Race.ANY, quantity=5,
                  classification=ItemClassification.trap),
-    item_names.TRAP_VOID_DUPLICATE:
-        ItemData(852 + SC2WOL_ITEM_ID_OFFSET, FactionlessItemType.VoidDuplicateTrap, 0, SC2Race.ANY, quantity=5,
+    item_names.MUTATOR_VOID_DUPLICATE:
+        ItemData(852 + SC2WOL_ITEM_ID_OFFSET, FactionlessItemType.MutatorVoidDuplicate, 0, SC2Race.ANY, quantity=5,
+                 classification=ItemClassification.trap),
+    item_names.MUTATOR_ENABLE_CLOAK:
+        ItemData(853 + SC2WOL_ITEM_ID_OFFSET, FactionlessItemType.MutatorEnableCloak, 0, SC2Race.ANY,
                  classification=ItemClassification.trap),
 
     # Nova gear
@@ -2093,7 +2097,7 @@ item_table = {
     item_names.MIRAGE_ETERNAL_DUTY: ItemData(458 + SC2LOTV_ITEM_ID_OFFSET, ProtossItemType.Item, 158, SC2Race.PROTOSS, parent=item_names.MIRAGE),
     item_names.MIRAGE_PHASE_ALIGNMENT: ItemData(459 + SC2LOTV_ITEM_ID_OFFSET, ProtossItemType.Item, 159, SC2Race.PROTOSS, parent=item_names.MIRAGE),
     item_names.MIRAGE_AFTERIMAGE: ItemData(460 + SC2LOTV_ITEM_ID_OFFSET, ProtossItemType.Item, 160, SC2Race.PROTOSS, parent=item_names.MIRAGE),
-    item_names.MIRAGE_OBSERVER_MODULE: ItemData(461 + SC2LOTV_ITEM_ID_OFFSET, ProtossItemType.Item, 161, SC2Race.PROTOSS, parent=item_names.MIRAGE),
+    item_names.MIRAGE_OBSERVER_MODULE: ItemData(461 + SC2LOTV_ITEM_ID_OFFSET, ProtossItemType.Item, 161, SC2Race.PROTOSS, classification=ItemClassification.progression, parent=item_names.MIRAGE),
     item_names.SKIRMISHER_ULTIMATE_SACRIFICE: ItemData(462 + SC2LOTV_ITEM_ID_OFFSET, ProtossItemType.Item, 162, SC2Race.PROTOSS, parent=item_names.SKIRMISHER),
     item_names.SKIRMISHER_RESOURCE_EFFICIENCY: ItemData(463 + SC2LOTV_ITEM_ID_OFFSET, ProtossItemType.Item, 163, SC2Race.PROTOSS, parent=item_names.SKIRMISHER),
     item_names.SKIRMISHER_ESSENCE_DRAIN: ItemData(464 + SC2LOTV_ITEM_ID_OFFSET, ProtossItemType.Item, 164, SC2Race.PROTOSS, parent=item_names.SKIRMISHER),
